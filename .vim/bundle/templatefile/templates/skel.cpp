dd5623f9811497de5f742bdd52955f2b80ebcd31
Bogdan-Cristian Tătăroiu <b.tataroiu@gmail.com>
2010-09-21 20:57:10 +0300

    Initial commit of dotfiles containing vim config.

---
+++ .vim/bundle/templatefile/templates/skel.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+    freopen("@FILE@.in", "rt", stdin);
+#ifndef DEBUG
+    freopen("@FILE@.out", "wt", stdout);
+#endif
+
+    return 0;
+}
