94ca256b15b6b9e9b9d331ffe42dc56b398ab71a
Bogdan-Cristian Tătăroiu <b.tataroiu@gmail.com>
2010-10-23 23:19:51 +0300

    Added support for .bashrc and .profile modifying
    To make it more platform independent, support for adding snippets quoted
    by comment markers was added.
    
    Fixed a bug where broken symlinks were not removed before trying to
    relink the packages properly.

---
+++ .bashrc
@@ -0,0 +1,38 @@
+# git status with a dirty flag
+function __git_status_flag {
+  git_status="$(git status 2> /dev/null)"
+  remote_pattern="^# Your branch is (.*) of"
+  diverge_pattern="# Your branch and (.*) have diverged"
+  if [[ ! ${git_status} =~ "working directory clean" ]]; then
+    state="⚡"
+    spacer=" "
+  fi
+
+  if [[ ${git_status} =~ ${remote_pattern} ]]; then
+    spacer=" "
+    if [[ ${BASH_REMATCH[1]} == "ahead" ]]; then
+      remote="↑"
+    else
+      remote="↓"
+    fi
+  fi
+
+  if [[ ${git_status} =~ ${diverge_pattern} ]]; then
+    remote="↕"
+    spacer=" "
+  fi
+
+  echo "${state}${remote}${spacer}"
+}
+
+export PS1='[\[\e[0;32m\]\u@\h \[\e[0;34m\]\w\[\033[00m\]]\[\e[22;35m\]$(__git_ps1 " [\[\e[33m\]$(__git_status_flag)\[\e[35m\]%s] ")\[\033[00m\]\$ '
+
+alias fix_permissions='sudo find . -executable -print0 | xargs -0 sudo chmod o+rx; sudo find . ! -executable -print0 | xargs -0 sudo chmod o+r'
+
+_pip_completion()
+{
+    COMPREPLY=( $( COMP_WORDS="${COMP_WORDS[*]}" \
+                   COMP_CWORD=$COMP_CWORD \
+                   PIP_AUTO_COMPLETE=1 $1 ) )
+}
+complete -o default -F _pip_completion pip
--- bootstrap.sh
+++ bootstrap.sh
@@ -4,15 +4,14 @@ REPOSITORY_PATH=$(dirname $(readlink -f $0))
 
 PACKAGES=".gitconfig .vim .vimrc"
 
-echo "Symlinking all packages"
+echo "Installing all symlink packages"
 for PACKAGE in $PACKAGES; do
+  if [ -h "$HOME/$PACKAGE" ]; then
+    rm "$HOME/$PACKAGE"
+  fi
   if [ -e "$HOME/$PACKAGE" ]; then
-    if [ -h "$HOME/$PACKAGE" ]; then
-      rm "$HOME/$PACKAGE"
-    else
-      echo "$HOME/$PACKAGE already exists."
-      continue
-    fi
+    echo "$HOME/$PACKAGE already exists."
+    continue
   fi
   ln -s "$REPOSITORY_PATH/$PACKAGE" "$HOME/$PACKAGE"
 done
@@ -23,5 +22,31 @@ ruby extconf.rb
 make
 rm Makefile mkmf.log *.o
 cd $OLDPWD
+
+echo "Installing all snippet packages"
+PACKAGES=".bashrc .profile"
+BEGIN_MARKER="# ---- DOT FILES BOOTSTRAPPING BEGIN ----"
+END_MARKER="# ---- DOT FILES BOOTSTRAPPING END ----"
+for PACKAGE in $PACKAGES; do
+  matched_begin_markers=$(grep -x -c "$BEGIN_MARKER" "$HOME/$PACKAGE")
+  matched_end_markers=$(grep -x -c "$END_MARKER" "$HOME/$PACKAGE")
+  matched_markers=$(($matched_begin_markers + $matched_end_markers))
+  if [ "$matched_begin_markers" == "1" -a "$matched_end_markers" == "1" ]; then
+    # Try and remove already installed snippet.
+    begin_line=$(grep -x -n "$BEGIN_MARKER" "$HOME/$PACKAGE")
+    begin_line=${begin_line%%:*}
+    end_line=$(grep -x -n "$END_MARKER" "$HOME/$PACKAGE")
+    end_line=${end_line%%:*}
+
+    sed -i "$begin_line,$end_line d" $HOME/$PACKAGE
+  elif [ "$matched_markers" != "0" ]; then
+    echo "Warning: unable to remove existing snippets in $PACKAGE"
+  fi
+
+  echo >> $HOME/$PACKAGE
+  echo "$BEGIN_MARKER" >> $HOME/$PACKAGE
+  cat $REPOSITORY_PATH/$PACKAGE >> $HOME/$PACKAGE
+  echo "$END_MARKER" >> $HOME/$PACKAGE
+done
 
 echo "Done. Have fun!"
