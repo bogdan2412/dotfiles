8e5dd2434150bd47c51415f2096571a221656446
Bogdan-Cristian Tătăroiu <b.tataroiu@gmail.com>
2015-12-06 14:31:59 +0000

    add awesomewm configs

---
+++ .config/awesome/mytheme.lua
@@ -0,0 +1,7 @@
+theme = dofile("/usr/share/awesome/themes/default/theme.lua")
+--theme.bg_urgent = "#ff0000"
+--theme.fg_urgent = "#ffffff"
+theme.wallpaper_cmd = { "true" }
+theme.font = "sans 10"
+theme.border_width = "1"
+return theme
