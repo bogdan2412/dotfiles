d15d2abc0dd812bc324a1b65b9ffca5057f64ae6
Bogdan-Cristian Tătăroiu <b.tataroiu@gmail.com>
2016-03-09 22:26:01 +0000

    Update emacs Ocaml config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -109,29 +109,45 @@
 
 (require 'scala-mode)
 
-;; Ocaml major mode
-(autoload 'tuareg-mode "tuareg" "Major mode for editing Caml code" t)
-(autoload 'camldebug "camldebug" "Run the Caml debugger" t)
-(autoload 'tuareg-imenu-set-imenu "tuareg-imenu"
-  "Configuration of imenu for tuareg" t)
-(add-hook 'tuareg-mode-hook 'tuareg-imenu-set-imenu)
-(setq auto-mode-alist
-      (append '(("\\.ml[ily]?$" . tuareg-mode)
-                ("\\.topml$" . tuareg-mode))
-              auto-mode-alist))
-
 ;; ML major mode
 (autoload 'sml-mode "sml-mode" "Major mode for editing SML." t)
 (add-to-list 'auto-mode-alist '("\\.sml$" . sml-mode))
 
 ;; Octave major mode
 (add-to-list 'auto-mode-alist '("\\.m$" . octave-mode))
+
+;; Ocaml major mode
+(autoload 'tuareg-mode "tuareg" "Major mode for editing Caml code" t)
+(autoload 'camldebug "camldebug" "Run the Caml debugger" t)
+(setq auto-mode-alist
+      (append '(("\\.ml[ily]?$" . tuareg-mode)
+                ("\\.topml$" . tuareg-mode))
+              auto-mode-alist))
+
+;; Merlin mode
+;; Add opam emacs directory to the load-path
+(setq opam-share (substring (shell-command-to-string "opam config var share 2> /dev/null") 0 -1))
+(add-to-list 'load-path (concat opam-share "/emacs/site-lisp"))
+;; Load merlin-mode
+(require 'merlin)
+;; Start merlin on ocaml files
+(add-hook 'tuareg-mode-hook 'merlin-mode t)
+(add-hook 'caml-mode-hook 'merlin-mode t)
+;; Enable auto-complete
+(setq merlin-use-auto-complete-mode 'easy)
+;; Use opam switch to lookup ocamlmerlin binary
+(setq merlin-command 'opam)
+
+;; Ocaml standard indenting
+(require 'ocp-indent)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ispell-program-name "/usr/local/bin/aspell"))
+ '(ispell-program-name "/usr/local/bin/aspell")
+ '(ocp-indent-config "JaneStreet"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
