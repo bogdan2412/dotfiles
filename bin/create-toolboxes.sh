41a639cabc07e6ffd1c28d4ecf6a6fb455c30527
Bogdan-Cristian Tătăroiu <b.tataroiu@gmail.com>
2019-12-27 10:32:20 +0000

    update toolbox scripts

--- bin/create-toolboxes.sh
+++ bin/create-toolboxes.sh
@@ -3,19 +3,33 @@
 set -euo pipefail
 
 toolbox create -c ocaml || true
-toolbox run -c ocaml sudo dnf upgrade -y --refresh
-toolbox run -c ocaml sudo dnf install -y emacs g++ libffi-devel opam openssl-devel vim zlib-devel zsh
-toolbox run -c ocaml opam init --compiler=4.07.1 --no-setup
+toolbox run -c ocaml sudo dnf upgrade --refresh -y
+toolbox run -c ocaml sudo dnf install -y emacs g++ libffi-devel opam openssl-devel pcre-devel vim zlib-devel zsh
+toolbox run -c ocaml sudo dnf autoremove -y
+toolbox run -c ocaml sudo dnf clean all
+toolbox run -c ocaml opam init --compiler=4.09.0 --no-setup
 
 toolbox create -c python || true
-toolbox run -c python sudo dnf upgrade -y --refresh
+toolbox run -c python sudo dnf upgrade --refresh -y
 toolbox run -c python sudo dnf install -y emacs python3-beautifulsoup4 python3-html5lib vim zsh
+toolbox run -c python sudo dnf autoremove -y
+toolbox run -c python sudo dnf clean all
 
 toolbox create -c wine || true
-toolbox run -c wine sudo dnf upgrade -y --refresh
-toolbox run -c wine sudo dnf install -y emacs wine vim zsh
+toolbox run -c wine sudo dnf upgrade --refresh -y
+toolbox run -c wine sudo dnf install -y wine
+toolbox run -c wine sudo dnf autoremove -y
+toolbox run -c wine sudo dnf clean all
 
 toolbox create -c media || true
-toolbox run -c media sudo dnf upgrade -y --refresh
+toolbox run -c media sudo dnf upgrade --refresh -y
 toolbox run -c media sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-toolbox run -c media sudo dnf install -y emacs ffmpeg plex-media-player vim youtube-dl zsh
+toolbox run -c media sudo dnf install -y ffmpeg plex-media-player xorg-x11-drv-nvidia unrar youtube-dl
+toolbox run -c media sudo dnf autoremove -y
+toolbox run -c media sudo dnf clean all
+
+toolbox create -c freerdp || true
+toolbox run -c freerdp sudo dnf upgrade --refresh -y
+toolbox run -c freerdp sudo dnf install -y freerdp
+toolbox run -c freerdp sudo dnf autoremove -y
+toolbox run -c freerdp sudo dnf clean all
