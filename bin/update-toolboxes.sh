0807b4d13b8eed2731963e9365f3c0a0c5d05aab
Bogdan-Cristian Tătăroiu <b.tataroiu@gmail.com>
2019-12-27 10:32:20 +0000

    update toolbox scripts

--- bin/update-toolboxes.sh
+++ bin/update-toolboxes.sh
@@ -3,6 +3,21 @@
 set -euo pipefail
 
 toolbox run -c ocaml sudo dnf upgrade --refresh -y
-toolbox run -c wine sudo dnf upgrade --refresh -y
+toolbox run -c ocaml sudo dnf autoremove -y
+toolbox run -c ocaml sudo dnf clean all
+
 toolbox run -c python sudo dnf upgrade --refresh -y
+toolbox run -c python sudo dnf autoremove -y
+toolbox run -c python sudo dnf clean all
+
+toolbox run -c wine sudo dnf upgrade --refresh -y
+toolbox run -c wine sudo dnf autoremove -y
+toolbox run -c wine sudo dnf clean all
+
 toolbox run -c media sudo dnf upgrade --refresh -y
+toolbox run -c media sudo dnf autoremove -y
+toolbox run -c media sudo dnf clean all
+
+toolbox run -c freerdp sudo dnf upgrade --refresh -y
+toolbox run -c freerdp sudo dnf autoremove -y
+toolbox run -c freerdp sudo dnf clean all
