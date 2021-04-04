00be7d13712f6a91f71681b38bfafde641a69cdb
Bogdan-Cristian Tătăroiu <b.tataroiu@gmail.com>
2021-04-04 16:48:05 +0100

    reorder toolboxes

--- bin/create-toolboxes.sh
+++ bin/create-toolboxes.sh
@@ -23,6 +23,22 @@ toolbox run -c ocaml sudo dnf autoremove -y
 toolbox run -c ocaml sudo dnf clean all
 toolbox run -c ocaml opam init --compiler=4.10.2 --no-setup
 
+toolbox create $CREATE_ARGS -c media || true
+toolbox run -c media sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+toolbox run -c media sudo dnf install -y beets beets-plugins cuetools ffmpeg flac ImageMagick plex-media-player shntool xorg-x11-drv-nvidia unrar youtube-dl
+toolbox run -c media sudo dnf autoremove -y
+toolbox run -c media sudo dnf clean all
+
+toolbox create $CREATE_ARGS -c node || true
+toolbox run -c node sudo dnf install -y alsa-lib jq libX11-xcb libXScrnSaver npm nss
+toolbox run -c node sudo dnf autoremove -y
+toolbox run -c node sudo dnf clean all
+
+toolbox create $CREATE_ARGS -c freerdp || true
+toolbox run -c freerdp sudo dnf install -y freerdp
+toolbox run -c freerdp sudo dnf autoremove -y
+toolbox run -c freerdp sudo dnf clean all
+
 toolbox create $CREATE_ARGS -c python || true
 toolbox run -c python sudo dnf install -y python3-beautifulsoup4 python3-html5lib python3-netifaces python3-pycodestyle
 toolbox run -c python sudo dnf autoremove -y
@@ -33,19 +49,3 @@ toolbox run -c wine sudo dnf install -y https://download1.rpmfusion.org/free/fed
 toolbox run -c wine sudo dnf install -y wine xorg-x11-drv-nvidia
 toolbox run -c wine sudo dnf autoremove -y
 toolbox run -c wine sudo dnf clean all
-
-toolbox create $CREATE_ARGS -c media || true
-toolbox run -c media sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-toolbox run -c media sudo dnf install -y beets beets-plugins cuetools ffmpeg flac ImageMagick plex-media-player shntool xorg-x11-drv-nvidia unrar youtube-dl
-toolbox run -c media sudo dnf autoremove -y
-toolbox run -c media sudo dnf clean all
-
-toolbox create $CREATE_ARGS -c freerdp || true
-toolbox run -c freerdp sudo dnf install -y freerdp
-toolbox run -c freerdp sudo dnf autoremove -y
-toolbox run -c freerdp sudo dnf clean all
-
-toolbox create $CREATE_ARGS -c node || true
-toolbox run -c node sudo dnf install -y alsa-lib jq libX11-xcb libXScrnSaver npm nss
-toolbox run -c node sudo dnf autoremove -y
-toolbox run -c node sudo dnf clean all
--- bin/update-toolboxes.sh
+++ bin/update-toolboxes.sh
@@ -6,6 +6,18 @@ toolbox run -c ocaml sudo dnf upgrade --refresh -y
 toolbox run -c ocaml sudo dnf autoremove -y
 toolbox run -c ocaml sudo dnf clean all
 
+toolbox run -c media sudo dnf upgrade --refresh -y
+toolbox run -c media sudo dnf autoremove -y
+toolbox run -c media sudo dnf clean all
+
+toolbox run -c node sudo dnf upgrade --refresh -y
+toolbox run -c node sudo dnf autoremove -y
+toolbox run -c node sudo dnf clean all
+
+toolbox run -c freerdp sudo dnf upgrade --refresh -y
+toolbox run -c freerdp sudo dnf autoremove -y
+toolbox run -c freerdp sudo dnf clean all
+
 toolbox run -c python sudo dnf upgrade --refresh -y
 toolbox run -c python sudo dnf autoremove -y
 toolbox run -c python sudo dnf clean all
@@ -13,15 +25,3 @@ toolbox run -c python sudo dnf clean all
 toolbox run -c wine sudo dnf upgrade --refresh -y
 toolbox run -c wine sudo dnf autoremove -y
 toolbox run -c wine sudo dnf clean all
-
-toolbox run -c media sudo dnf upgrade --refresh -y
-toolbox run -c media sudo dnf autoremove -y
-toolbox run -c media sudo dnf clean all
-
-toolbox run -c freerdp sudo dnf upgrade --refresh -y
-toolbox run -c freerdp sudo dnf autoremove -y
-toolbox run -c freerdp sudo dnf clean all
-
-toolbox run -c node sudo dnf upgrade --refresh -y
-toolbox run -c node sudo dnf autoremove -y
-toolbox run -c node sudo dnf clean all
