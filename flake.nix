83cfe4cfabb6527e24f893f9dab6f8c41bd4f139
Bogdan-Cristian Tătăroiu <b.tataroiu@gmail.com>
2025-12-29 20:31:17 +0000

    Setup [nix] shell with lua LSP servers.

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            lua-language-server
+            stylua
+          ];
+        };
+
+        formatter = pkgs.nixfmt-tree;
+      }
+    );
+}
